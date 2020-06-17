afc08a6072e7ca487d29719f7e97f62ebc32d3a1
Robert Quintana <robertquintanatx@gmail.com>
2020-06-17 20:02:23 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -20,8 +20,8 @@ end
 
 #projects: kickstarter.css("li.project.grid_4")
 #title: project.css("li.project.grid_4")
-#image link: projects.css("div.project-thumbnail a img").attribute("src").value
+#image link: project.css("div.project-thumbnail a img").attribute("src").value
 #description: project.css("p.bbcard_blurb")
-#location: projects.css("span.location-name")
+#location: project.css("span.location-name")
 #percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%","")
 create_project_hash
