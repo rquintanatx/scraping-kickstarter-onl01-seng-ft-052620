ac62787d2226aa6912255f2cc73fee196e1aeb34
Robert Quintana <robertquintanatx@gmail.com>
2020-06-17 17:16:24 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,8 @@
 # require libraries/modules here
 
 def create_project_hash
-  # write your code here
+  html = 
+  File.read('fixtuures/kickstarter.html')
+  
+  kickstarter = Nokogiri::HTML(html)
 end
