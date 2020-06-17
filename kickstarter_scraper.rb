c1419b2112073ebc517c3a6cb2cc1404268fbc6c
Robert Quintana <robertquintanatx@gmail.com>
2020-06-17 19:46:10 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,5 +11,5 @@ end
 #projects: kickstarter.css("li.project.grid_4")
 #title: project.css("li.project.grid_4")
 #image link: projects.css("div.project-thumbnail a img").attribute("src").value
-
+#description: project.css("p.bbcard_blurb")
 create_project_hash
